72850b8517846b1a68b08f646e864dd0045b9d81
Prosenjit Chowdhury <pc.chy007@gmail.com>
2025-12-18 23:11:25 +0600

    session-7: added server

---
+++ session-7/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello from the server");
+})
+
+app.get("/two", (request, response) => {
+    response.send("Hello from the second page of the server");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+})
+
+
+/**
+ * For example: www.undpvolunteer.com
+ * Server: www.api.undpvolunteer.com
+ */
